576103eb15caf8a5a02b33277ef5afe900c08795
Kyle Snow <kjsnow11@gmail.com>
2018-07-16 16:12:00 -0400

    dynamic start and end date variables

--- automated_process_ann_train.py
+++ automated_process_ann_train.py
@@ -44,8 +44,11 @@ def main():
 	user = 'ksnow'
 	pwd = 'Kk5N03!w'
 
-	start_date = dt.date(2018,6,10)
-	end_date = dt.date(2018,6,13)
+	#start_date =  dt.datetime.now().date() - dt.timedelta(days=5)
+	#end_date = start_date + dt.timedelta(days=2)
+	# different date range
+	start_date = dt.date(2018,5,10)
+	end_date = dt.date(2018,5,13)
 
 	#, (DATEPART(HOUR, churn_date) * 3600 + DATEPART(MINUTE, churn_date) * 60 + DATEPART(SECOND, churn_date)) as sec_from_midnight
 	# query = 'select tn, case when churn_losing_sp = 1 then 1 else 0 end as is_vzw, case when churn_losing_sp = 2 then 1 else 0 end as is_att, case when churn_losing_sp = 3 then 1 else 0 end as is_spr, case when churn_losing_sp = 4 then 1 else 0 end as is_tmo, case when churn_losing_sp = 6 then 1 else 0 end as is_met, case when datepart(hour, churn_date) >= 21 or datepart(hour, churn_date) < 9 then 1 else 0 end as night_ind, case when datepart(minute, churn_date) % 5 = 0 and datepart(second, churn_date) = 0 then 1 else 0 end as five_min_ind, case when noncompetitive_ind = 5 then 1 else 0 end as noncomp_ind from com_cdm_stage..f_final_churn_reporting where churn_date >= \'2018-06-04\' and churn_date < \'2018-06-05\' and churn_losing_sp in (1,2,3,4,6) and churn_type like \'%DSV\' and noncompetitive_ind in (0,5)'
@@ -53,9 +56,54 @@ def main():
 	# (DATEPART(HOUR, a.churn_date) * 3600 + DATEPART(MINUTE, a.churn_date) * 60 + DATEPART(SECOND, a.churn_date)) as sec_from_midnight,
 
 	# Make sure to update when changing queries
-	col_list = ['tn', 'is_vzw', 'is_att', 'is_spr', 'is_tmo', 'is_met', 'night_ind', 'five_min_ind', 'count_same_minute', 'count_same_second', 'noncomp_ind']
+	#col_list = ['tn', 'is_vzw', 'is_att', 'is_spr', 'is_tmo', 'is_met', 'night_ind', 'five_min_ind', 'count_same_minute', 'count_same_second', 'noncomp_ind']
 
-	query = 'with cte as (select tn, churn_losing_sp, churn_date, noncompetitive_ind from com_cdm_stage..f_final_churn_reporting where churn_date >= \'2018-06-10\' and churn_date < \'2018-06-13\' and churn_losing_sp in (1,2,3,4,6) and churn_type like \'%DSV\' and noncompetitive_ind in (0,5)),cte_minute as (select churn_losing_sp, datepart(day, churn_date) as cday, datepart(hour,churn_date) as chour, datepart(minute,churn_date) as cminute, count(1) as count_same_minute from cte group by churn_losing_sp, datepart(day, churn_date), datepart(hour,churn_date), datepart(minute,churn_date)),cte_second as (select churn_losing_sp, datepart(day, churn_date) as cday, datepart(hour,churn_date) as chour, datepart(minute,churn_date) as cminute, datepart(second,churn_date) as csecond, count(1) as count_same_second from cte group by churn_losing_sp, datepart(day, churn_date), datepart(hour,churn_date), datepart(minute,churn_date), datepart(second,churn_date)) select a.tn,case when a.churn_losing_sp = 1 then 1 else 0 end as is_vzw, case when a.churn_losing_sp = 2 then 1 else 0 end as is_att, case when a.churn_losing_sp = 3 then 1 else 0 end as is_spr, case when a.churn_losing_sp = 4 then 1 else 0 end as is_tmo, case when a.churn_losing_sp = 6 then 1 else 0 end as is_met,case when datepart(hour, a.churn_date) >= 21 or datepart(hour, a.churn_date) < 9 then 1 else 0 end as night_ind,case when datepart(minute, a.churn_date) % 5 = 0 and datepart(second, a.churn_date) = 0 then 1 else 0 end as five_min_ind,m.count_same_minute,s.count_same_second,case when a.noncompetitive_ind = 5 then 1 else 0 end as noncomp_ind from cte a inner join cte_minute m on a.CHURN_LOSING_SP = m.CHURN_LOSING_SP and datepart(day, a.churn_date) = m.cday and datepart(hour,a.churn_date) = m.chour and datepart(minute,a.churn_date) = m.cminute inner join cte_second s on a.CHURN_LOSING_SP = s.CHURN_LOSING_SP and datepart(day, a.churn_date) = s.cday and datepart(hour,a.churn_date) = s.chour and datepart(minute,a.churn_date) = s.cminute and datepart(second,a.churn_date) = s.csecond order by a.churn_losing_sp, a.churn_date'
+	#query = 'with cte as (select tn, churn_losing_sp, churn_date, noncompetitive_ind from com_cdm_stage..f_final_churn_reporting where churn_date >= \'2018-06-10\' and churn_date < \'2018-06-13\' and churn_losing_sp in (1,2,3,4,6) and churn_type like \'%DSV\' and noncompetitive_ind in (0,5)),cte_minute as (select churn_losing_sp, datepart(day, churn_date) as cday, datepart(hour,churn_date) as chour, datepart(minute,churn_date) as cminute, count(1) as count_same_minute from cte group by churn_losing_sp, datepart(day, churn_date), datepart(hour,churn_date), datepart(minute,churn_date)),cte_second as (select churn_losing_sp, datepart(day, churn_date) as cday, datepart(hour,churn_date) as chour, datepart(minute,churn_date) as cminute, datepart(second,churn_date) as csecond, count(1) as count_same_second from cte group by churn_losing_sp, datepart(day, churn_date), datepart(hour,churn_date), datepart(minute,churn_date), datepart(second,churn_date)) select a.tn,case when a.churn_losing_sp = 1 then 1 else 0 end as is_vzw, case when a.churn_losing_sp = 2 then 1 else 0 end as is_att, case when a.churn_losing_sp = 3 then 1 else 0 end as is_spr, case when a.churn_losing_sp = 4 then 1 else 0 end as is_tmo, case when a.churn_losing_sp = 6 then 1 else 0 end as is_met,case when datepart(hour, a.churn_date) >= 21 or datepart(hour, a.churn_date) < 9 then 1 else 0 end as night_ind,case when datepart(minute, a.churn_date) % 5 = 0 and datepart(second, a.churn_date) = 0 then 1 else 0 end as five_min_ind,m.count_same_minute,s.count_same_second,case when a.noncompetitive_ind = 5 then 1 else 0 end as noncomp_ind from cte a inner join cte_minute m on a.CHURN_LOSING_SP = m.CHURN_LOSING_SP and datepart(day, a.churn_date) = m.cday and datepart(hour,a.churn_date) = m.chour and datepart(minute,a.churn_date) = m.cminute inner join cte_second s on a.CHURN_LOSING_SP = s.CHURN_LOSING_SP and datepart(day, a.churn_date) = s.cday and datepart(hour,a.churn_date) = s.chour and datepart(minute,a.churn_date) = s.cminute and datepart(second,a.churn_date) = s.csecond order by a.churn_losing_sp, a.churn_date'
+
+		#--(DATEPART(HOUR, a.churn_date) * 3600 + DATEPART(MINUTE, a.churn_date) * 60 + DATEPART(SECOND, --a.churn_date)) as sec_from_midnight,
+
+	query = """
+				with cte as (
+	select tn, churn_losing_sp, churn_date, noncompetitive_ind 
+	from com_cdm_stage..f_final_churn_reporting where churn_date >= '{0}' and churn_date < '{1}' and churn_losing_sp in (1,2,3,4,6) and churn_type like '%DSV' and noncompetitive_ind in (0,5)
+	),
+	cte_minute as (
+	select churn_losing_sp, datepart(day, churn_date) as cday, datepart(hour,churn_date) as chour, datepart(minute,churn_date) as cminute, count(1) as count_same_minute 
+	from cte
+	group by churn_losing_sp, datepart(day, churn_date), datepart(hour,churn_date), datepart(minute,churn_date)
+	),
+	cte_second as (
+	select churn_losing_sp, datepart(day, churn_date) as cday, datepart(hour,churn_date) as chour, datepart(minute,churn_date) as cminute, datepart(second,churn_date) as csecond, count(1) as count_same_second 
+	from cte
+	group by churn_losing_sp, datepart(day, churn_date), datepart(hour,churn_date), datepart(minute,churn_date), datepart(second,churn_date) 
+	)
+	select a.tn, 
+	case when a.churn_losing_sp = 1 then 1 else 0 end as is_vzw, 
+	case when a.churn_losing_sp = 2 then 1 else 0 end as is_att, 
+	case when a.churn_losing_sp = 3 then 1 else 0 end as is_spr, 
+	case when a.churn_losing_sp = 4 then 1 else 0 end as is_tmo, 
+	case when a.churn_losing_sp = 6 then 1 else 0 end as is_met,
+	case when datepart(hour, a.churn_date) >= 21 or datepart(hour, a.churn_date) < 9 then 1 else 0 end as night_ind,
+	case when datepart(minute, a.churn_date) % 5 = 0 and datepart(second, a.churn_date) = 0 then 1 else 0 end as five_min_ind,
+	m.count_same_minute,
+	s.count_same_second,
+	case when a.noncompetitive_ind = 5 then 1 else 0 end as noncomp_ind 
+	from cte a
+	inner join cte_minute m
+	on a.CHURN_LOSING_SP = m.CHURN_LOSING_SP
+	and datepart(day, a.churn_date) = m.cday
+	and datepart(hour,a.churn_date) = m.chour
+	and datepart(minute,a.churn_date) = m.cminute
+	inner join cte_second s
+	on a.CHURN_LOSING_SP = s.CHURN_LOSING_SP
+	and datepart(day, a.churn_date) = s.cday
+	and datepart(hour,a.churn_date) = s.chour
+	and datepart(minute,a.churn_date) = s.cminute
+	and datepart(second,a.churn_date) = s.csecond
+	order by a.churn_losing_sp, a.churn_date
+	""".format(start_date, end_date)
+
+	col_list = ['tn', 'is_vzw', 'is_att', 'is_spr', 'is_tmo', 'is_met', 'night_ind', 'five_min_ind', 'count_same_minute', 'count_same_second', 'noncomp_ind']
 
 	sample = hf.fetch_data(server=server,db=db,user=user,pwd=pwd,query=query, col_list=col_list)
 
@@ -66,7 +114,7 @@ def main():
 
 	Y = Y.astype(np.int32)
 
-	M = 5 # hidden nodes
+	M = 7 # hidden nodes
 	D = X.shape[1] # number of features
 	K = len(set(Y))
 
@@ -89,7 +137,9 @@ def main():
 	# Initialize cost array and learning_rate
 	train_costs = []
 	test_costs = []
-	learning_rate = .00001
+	# 96%, bumpy
+	learning_rate = .000001
+
 
 	for i in xrange(1000):
 		pYtrain, Ztrain = forward(Xtrain, W1, b1, W2, b2)
